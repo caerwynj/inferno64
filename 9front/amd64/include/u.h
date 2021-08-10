e16b4d85dd6b1cf14834387b765113114dabeae2
9ferno <gophone2015@gmail.com>
2021-08-10 15:21:12 -0700

    added missing type definitions

--- 9front/amd64/include/u.h
+++ 9front/amd64/include/u.h
@@ -1,3 +1,12 @@
 #include "/amd64/include/u.h"
 typedef intptr WORD;
 typedef uintptr        UWORD;
+
+typedef unsigned char	u8;
+typedef unsigned short	u16;
+typedef unsigned int	u32;
+typedef unsigned long long u64;
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
