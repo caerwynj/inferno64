2f511a57a371cc1931125ceaf80f1c79eb30e5a0
Caerwyn Jones <caerwynj@gmail.com>
2026-02-28 19:20:17 -0500

    mv spree back to appl

--- recycle/appl/spree/joinsession.m
+++
@@ -1,7 +0,0 @@
-
-Joinsession: module {
-	PATH: con "/dis/spree/joinsession.dis";
-	join: fn(ctxt: ref Draw->Context, mnt: string, dir: string, join: string): string;
-	init:   fn(ctxt: ref Draw->Context, argv: list of string);
-};
-
